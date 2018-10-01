fb787fb48ae6e879b975390082779a46226e727d
m2vang <m.vang02@yahoo.com>
2018-10-01 07:48:52 -0500

    Adding and deleting services works.

--- server/routes/chart.router.js
+++ server/routes/chart.router.js
@@ -3,18 +3,85 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const chartQuery = `SELECT "service_name", 
+    if (req.isAuthenticated()) {
+        const chartQuery = `SELECT "service_name", 
                         COUNT("service_name") 
                         FROM "service_types" 
                         JOIN "calendar" 
                         ON "service_types"."id" = "calendar"."service_types_id" 
                         GROUP BY "service_name";`;
-    pool.query(chartQuery)
+        pool.query(chartQuery)
+            .then(result => res.send(result.rows))
+            .catch(error => {
+                console.log('error in GET chart', error);
+                res.sendStatus(403);
+            });
+    } else {
+        res.sendStatus(403);
+    }; //end of if-else auth
+}); //end of GET
+
+router.get('/services', (req, res) => {
+    if (req.isAuthenticated()) {
+        const serviceQuery = `SELECT "service_types"."id", "service_name", "category_types"."category" 
+                        FROM "service_types" 
+                        JOIN "category_types" 
+                        ON "service_types"."category_types_id" = "category_types"."id";`;
+        pool.query(serviceQuery)
+            .then(result => res.send(result.rows))
+            .catch(error => {
+                console.log('error in GET chart', error);
+                res.sendStatus(403);
+            });
+    } else {
+        res.sendStatus(403);
+    }; //end of if-else auth
+}); //end of GET
+
+router.delete('/removeService/:id', (req, res) => {
+    console.log('in DELETE /removeService Route', req.params.id);
+    if (req.isAuthenticated()) {
+        const idToDelete = req.params.id;
+        const deleteServiceQuery = `DELETE FROM "service_types" WHERE "id" = $1;`;
+        pool.query(deleteServiceQuery, [idToDelete])
+            .then((result) => {
+                res.sendStatus(200);
+            }).catch((error) => {
+                console.log('error in DELETE /removeService Route', error);
+                res.sendStatus(500)
+            });
+    } else {
+        res.sendStatus(403);
+    }; //end of if-else auth
+}); //end of DELETE
+
+router.post('/addService', (req, res) => {
+    console.log('in POST /addService Route', req.body);
+    if (req.isAuthenticated()) {
+        const categoryToAdd = req.body.newCategory;
+        const serviceToAdd = req.body.newService;
+        const durationToAdd = req.body.newDuration;
+        const addServiceQuery = `INSERT INTO "service_types" ("category_types_id", "service_name", "duration")
+                                    VALUES ($1, $2, $3);`;
+        pool.query(addServiceQuery, [categoryToAdd, serviceToAdd, durationToAdd])
+            .then((result) => {
+                res.send(result.rows);
+            }).catch((error) => {
+                console.log('Error in POST /addService', error);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403);
+    }; //end of if-else
+}); //end of POST
+
+//handles getting all of the different categories
+router.get('/category', (req, res) => {
+    const catQuery = `SELECT * FROM "category_types" ORDER BY "id";`;
+    pool.query(catQuery)
         .then(result => res.send(result.rows))
         .catch(error => {
-            console.log('error in GET chart', error);
-            res.sendStatus(403);
+            console.log('error in GET /category', error);
         });
-});
-
+}); //end of GET
 module.exports = router;
--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -8,6 +8,20 @@ import Nav from '../../components/Nav/Nav';
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 import swal from 'sweetalert';
 import Button from '@material-ui/core/Button';
+//Material Table
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+//Import Material Expansion Table
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+
+import TextField from '@material-ui/core/TextField';
+
 
 const mapStateToProps = state => ({
   user: state.user,
@@ -20,11 +34,18 @@ class InfoPage extends Component {
       user: { ...this.props.user },
       edit: false,
       message: '',
+      services: [],
+      newService: '',
+      newCategory: '',
+      newDuration: '',
+      category: [],
     }
   } //end of constructor
 
   componentDidMount() {
     this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
+    this.getServices();
+    this.getCategory();
   } //end of componentDidMount()
 
   componentDidUpdate() {
@@ -101,6 +122,73 @@ class InfoPage extends Component {
     } //end of if-else
   } //end of InfoPage class
 
+  getServices() {
+    axios.get('/api/chart/services/')
+      .then((response) => {
+        this.setState({
+          services: response.data
+        })
+      }).catch((error) => {
+        console.log('Error in getServices', error);
+        alert('Cannot get services!');
+      }); //end of axios
+  } //end of getServices
+
+  removeService = (id) => {
+    swal("Service Deleted!", "You will no longer offer this service!", "success");
+    axios.delete('/api/chart/removeService/' + id)
+      .then((response) => {
+        this.getServices();
+      }).catch((error) => {
+        swal("Unable to delete service!", "Cannot delete service!", "failure");
+        console.log('Error in removeService', error);
+      }); //end of axios
+  } //end of removeService
+
+  handleInputChangeFor = propertyName => (event) => {
+    this.setState({
+      [propertyName]: event.target.value,
+    });
+  }
+
+  addService = (event) => {
+    event.preventDefault();
+    swal("Service Added!", "You've added a new service!", "success");
+    if (this.state.newCategory === '' || this.state.newService === '' || this.state.newDuration === '') {
+      this.setState({
+        message: 'Please fill in all fields!',
+      })
+    } else {
+      const body = {
+        newCategory: this.state.newCategory,
+        newService: this.state.newService,
+        newDuration: this.state.newDuration
+      };
+
+      axios.post('/api/chart/addService/', body)
+        .then((response) => {
+          this.getServices();
+        }).catch((error) => {
+          console.log('Error in addService', error);
+          alert('Unable to add service!');
+        }); //end of axios
+    } //end of if/else
+
+    //empty input field
+
+  } //end of addService
+
+  getCategory() {
+    axios.get('/api/chart/category/')
+      .then((response) => {
+        this.setState({
+          category: response.data
+        })
+      }).catch((error) => {
+        console.log('error in getCategory!', error);
+      })
+  }
+
   render() {
     let content = null;
 
@@ -108,62 +196,132 @@ class InfoPage extends Component {
     if (this.state.edit === false) {
       content = (
         <div className="info">
-            <h4>First Name: {this.props.user.first_name}</h4>
-            <h4>Last Name: {this.props.user.last_name}</h4>
-            <h4>Telephone: {this.props.user.telephone}</h4>
+          <h4>First Name: {this.props.user.first_name}</h4>
+          <h4>Last Name: {this.props.user.last_name}</h4>
+          <h4>Telephone: {this.props.user.telephone}</h4>
           <Button className="button" variant="outlined" color="primary" onClick={this.edit}>Edit</Button>
         </div>
       ) //end of content for edit=false
     } else { //otherwise show this on DOM
       content = (
         <div className="newInfo">
-            <h1>Edit Account</h1>
-            <div>
-              <label htmlFor="first_name">
-                First Name:
+          <h1>Edit Account</h1>
+          <div>
+            <label htmlFor="first_name">
+              First Name:
               <input
-                  className="newInput"
-                  type="text"
-                  name="first_name"
-                  value={this.props.user.first_name}
-                  onChange={this.handleInputChangeFor('first_name')}
-                />
-              </label>
-            </div>
-            <div>
-              <label htmlFor="last_name">
-                Last Name:
+                className="newInput"
+                type="text"
+                name="first_name"
+                value={this.props.user.first_name}
+                onChange={this.handleInputChangeFor('first_name')}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="last_name">
+              Last Name:
               <input
-                  className="newInput"
-                  type="text"
-                  name="last_name"
-                  value={this.props.user.last_name}
-                  onChange={this.handleInputChangeFor('last_name')}
-                />
-              </label>
-            </div>
-            <div>
-              <label htmlFor="telephone">
-                Telephone:
+                className="newInput"
+                type="text"
+                name="last_name"
+                value={this.props.user.last_name}
+                onChange={this.handleInputChangeFor('last_name')}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="telephone">
+              Telephone:
               <input
-                  className="newInput"
-                  country="US"
-                  name="telephone"
-                  value={this.props.user.telephone}
-                  onChange={this.handleInputChangeFor('telephone')}
-                />
-              </label>
-            </div>
-          <Button className="button"variant="outlined" color="primary" onClick={this.updateUser}>Save</Button>
+                className="newInput"
+                country="US"
+                name="telephone"
+                value={this.props.user.telephone}
+                onChange={this.handleInputChangeFor('telephone')}
+              />
+            </label>
+          </div>
+          <Button className="button" variant="outlined" color="primary" onClick={this.updateUser}>Save</Button>
           <Button variant="outlined" color="secondary" onClick={this.cancelEdit}>Cancel</Button>
         </div>
       ) //end of content for edit=true
     } //end of if-else
 
+    let serviceTypeData = null;
+    if (this.props.user.if_stylist === true) {
+      serviceTypeData = (
+        <div>
+          <form>
+            <h1>Add a new Service</h1>
+            <select onChange={this.handleInputChangeFor('newCategory')}>
+              {this.state.category.map((category, index) => {
+                return (
+                  <option key={index} value={category.id}>{category.category}</option>
+                )
+              })}
+            </select>
+            <label htmlFor="service_name">
+              Service Name:
+              <input
+                type="text"
+                name="service_name"
+                value={this.state.newService}
+                onChange={this.handleInputChangeFor('newService')}
+              />
+            </label>
+            <label htmlFor="duration">
+              Duration:
+              <input
+                type="number"
+                name="duration"
+                value={this.state.newDuration}
+                onChange={this.handleInputChangeFor('newDuration')}
+              />
+            </label>
+            <Button variant="outlined" color="primary" onClick={this.addService}>Add</Button>
+          </form>
+          <ExpansionPanel>
+            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+              <p>Services</p>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Category Type</TableCell>
+                    <TableCell>Service Name</TableCell>
+                    <TableCell>Actions</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {this.state.services.map((service, index) => {
+                    return (
+                      <TableRow key={index}>
+                        <TableCell>{service.category}</TableCell>
+                        <TableCell>{service.service_name}</TableCell>
+                        <TableCell><Button variant="outlined" color="secondary" onClick={() => this.removeService(service.id)}>Delete</Button></TableCell>
+                      </TableRow>
+                    )
+                  })}
+                </TableBody>
+              </Table>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        </div>
+      )
+    } else {
+      serviceTypeData = (
+        <p></p>
+      )
+    }
+
     return (
       <div>
         <Nav />
         {content}
+        <br />
+        {serviceTypeData}
       </div>
     ); //end of return
   } //end of render
